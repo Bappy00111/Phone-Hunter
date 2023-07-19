949799d8d5ab32936734bfe7b9935bf275d43635
“[bappy <[bappymodok0011@gamil.com]>
2023-07-18 23:27:37 -0700

    spinner added

--- js/index.js
+++ js/index.js
@@ -2,15 +2,32 @@ const loadPhones = async(suschFiled) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${suschFiled}`;
     const res = await fetch(url)
     const data = await res.json()
-    showPhones(data.data)
-    
+    showPhones(data.data.slice(0,21))
+   
 }
 
 const showPhones = (phones) =>{
     const phoneContainer = document.getElementById('phone-container')
-    phoneContainer.innerText = " "
+    phoneContainer.innerText = " ";
+    // display 20 phone only 
+    // phones = phones.slice(0,3)
+    
+    // display no Phone found 
+    const noPhone = document.getElementById('no-phone-message')
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none')
+    }
+    else{
+        noPhone.classList.add('d-none')
+    }
+    // display button all 
+   
+   
+
+
+    // display all phone 
     phones.forEach(phone => {
-        console.log(phone.phone_name)
+        console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
@@ -25,12 +42,34 @@ const showPhones = (phones) =>{
         phoneContainer.appendChild(div)
        
     });
+
+    // stop loader spinner
+    toggleSpinner(false)
 }
- 
+
+
+//  handle Susch button click 
 const buttonSusch = () =>{
+    // start loader 
+    toggleSpinner(true)
     const suschFiled = document.getElementById('susch-filed').value;
     loadPhones(suschFiled)
+    
 
 }
 
-loadPhones();
+// toggle spinner 
+const toggleSpinner = isLoging =>{
+    const loaderSection = document.getElementById('loader')
+    if(isLoging){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none')
+    }
+}
+
+
+
+
+// loadPhones();
